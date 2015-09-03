efdb632cffbaff72556bf955c0be88c00cdd3930
Jake Spurlock <jake_spurlock@wired.com>
2015-09-03 15:17:39 -0700

    Check against podcast post types.

--- includes/class-ssp-admin.php
+++ includes/class-ssp-admin.php
@@ -897,7 +897,7 @@ class SSP_Admin {
 	 */
 	public function invalidate_cache( $id, $post ){
 
-		if ( in_array( $post->post_type, array( 'posts', ) ) ){
+		if ( in_array( $post->post_type, ssp_post_types( true ) ) ){
 			wp_cache_delete( 'episodes', 'ssp' );
 			wp_cache_delete( 'episodes', 'episode_ids' );
 		}

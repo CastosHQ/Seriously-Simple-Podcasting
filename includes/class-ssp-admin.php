aa1eefea263443046db3f43c65fc6f99a2add704
Hugh Lashbrooke <hlashbrooke@gmail.com>
2016-08-30 13:43:23 +0200

    Fixing object to array designation

--- includes/class-ssp-admin.php
+++ includes/class-ssp-admin.php
@@ -255,7 +255,7 @@ class SSP_Admin {
 
     		$args = array(
 				'type' => 'string',
-				'description' => $data->meta_description,
+				'description' => $data['meta_description'],
 				'single' => true,
 				'show_in_rest' => true,
 			);

039fb174faa8ce7e07b6242b9c60880b3ace3689
Jonathan Bossenger <jonathanbossenger@gmail.com>
2017-12-13 22:12:54 +0200

    Refactored the get_album_art function

--- includes/class-ssp-frontend.php
+++ includes/class-ssp-frontend.php
@@ -317,61 +317,110 @@ class SSP_Frontend {
 	 */
 	public function get_album_art( $episode_id = false ) {
 		
-		if ( ! $episode_id ) {
-			$src    = SSP_PLUGIN_URL . '/assets/images/no-album-art.png';
-			$width  = 300;
-			$height = 300;
-			
-			return compact( 'src', 'width', 'height' );
+		/**
+		 * In case the episode id is not passed
+		 */
+		if (!$episode_id){
+			return $this->get_no_album_art_image_array();
 		}
 		
+		$image_data_array = array();
+		
+		/**
+		 * Option 1 : if the episode has a featured image that is square, then use that
+		 */
+		$thumb_id = get_post_thumbnail_id( $episode_id );
+		if ( ! empty( $thumb_id ) ) {
+			$image_data_array = $this->return_renamed_image_array_keys( wp_get_attachment_image_src( $thumb_id, 'medium' ) );
+			if ( $this->check_image_is_square( $image_data_array ) ) {
+				return $image_data_array;
+			}
+		}
+		
+		/**
+		 * Option 2: if the episode belongs to a series, which has an image that is square, then use that
+		 */
 		$series_id = false;
 		
-		if ( $series = get_the_terms( $episode_id, 'series' ) ) {
+		$series = get_the_terms( $episode_id, 'series' );
+		if ( $series ) {
 			$series_id = ( ! empty( $series ) && isset( $series[0] ) ) ? $series[0]->term_id : false;
 		}
-		
-		if ( $series_id && $series_image = get_option( "ss_podcasting_data_image_{$series_id}" ) ) {
-			$series_image_attachment_id = ssp_get_image_id_from_url( $series_image );
-			list( $src, $width, $height ) = wp_get_attachment_image_src( $series_image_attachment_id, 'medium' );
-			
-			if ( ( $width / $height ) !== 1 ) {
-				if ( $series_image = get_option( "ss_podcasting_data_image" ) ) {
-					$series_image_attachment_id = ssp_get_image_id_from_url( $series_image );
-					list( $src, $width, $height ) = wp_get_attachment_image_src( $series_image_attachment_id, 'medium' );
-					
-					if ( ( $width / $height ) !== 1 ) {
-						$src    = SSP_PLUGIN_URL . '/assets/images/no-album-art.png';
-						$width  = 300;
-						$height = 300;
-						
-						return compact( 'src', 'width', 'height' );
-					}
-					
-					return compact( 'src', 'width', 'height' );
-				}
-			}
-			
-			return compact( 'src', 'width', 'height' );
-		} elseif ( !$series_id && $series_image = get_option( "ss_podcasting_data_image" ) ) {
-			$series_image_attachment_id = ssp_get_image_id_from_url( $series_image );
-			list( $src, $width, $height ) = wp_get_attachment_image_src( $series_image_attachment_id, 'medium' );
-			if ( ( $width / $height ) !== 1 ) {
-				$src    = SSP_PLUGIN_URL . '/assets/images/no-album-art.png';
-				$width  = 300;
-				$height = 300;
-			}
-			
-			return compact( 'src', 'width', 'height' );
-		} else {
-			$src    = SSP_PLUGIN_URL . '/assets/images/no-album-art.png';
-			$width  = 300;
-			$height = 300;
-			
-			return compact( 'src', 'width', 'height' );
+		if ( $series_id ) {
+			$series_image = get_option( "ss_podcasting_data_image_{$series_id}", false );
 		}
+		if ( $series_image ) {
+			$series_image_attachment_id = ssp_get_image_id_from_url( $series_image );
+			$image_data_array = $this->return_renamed_image_array_keys( wp_get_attachment_image_src( $series_image_attachment_id, 'medium' ) );
+			if ( $this->check_image_is_square( $image_data_array ) ) {
+				return $image_data_array;
+			}
+		}
+		
+		/**
+		 * Option 3: if the feed settings have an image that is square, then use that
+		 */
+		$feed_image = get_option( 'ss_podcasting_data_image', false );
+		if ( $feed_image ) {
+			$feed_image_attachment_id = ssp_get_image_id_from_url( $feed_image );
+			$image_data_array = $this->return_renamed_image_array_keys( wp_get_attachment_image_src( $feed_image_attachment_id, 'medium' ) );
+			if ( $this->check_image_is_square( $image_data_array ) ) {
+				return $image_data_array;
+			}
+		}
+		
+		/**
+		 * None of the above passed, return the no-album-art image
+		 */
+		return $this->get_no_album_art_image_array();
 	}
-
+	
+	/**
+	 * Convert the array returned from wp_get_attachment_image_src into a human readable version
+	 * @todo check if there is a WordPress function for this
+	 *
+	 * @param $image_data_array
+	 *
+	 * @return mixed
+	 */
+	private function return_renamed_image_array_keys($image_data_array){
+		if ( $image_data_array && ! empty( $image_data_array ) ) {
+			$new_image_data_array['src']    = isset($image_data_array[0]) ? $image_data_array[0] : '' ;
+			$new_image_data_array['width']  = isset($image_data_array[1]) ? $image_data_array[1] : '' ;
+			$new_image_data_array['height'] = isset($image_data_array[2]) ? $image_data_array[2] : '' ;
+		}
+		return $new_image_data_array;
+	}
+	
+	/**
+	 * Check if the image in the formatted image_data_array is a square image
+	 *
+	 * @param array $image_data_array
+	 *
+	 * @return bool
+	 */
+	private function check_image_is_square( $image_data_array = array() ) {
+		if ( isset( $image_data_array['width'] ) && isset( $image_data_array['height'] ) ) {
+			if ( ( $image_data_array['width'] / $image_data_array['height'] ) === 1 ) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	/**
+	 * Returns the no album art image
+	 *
+	 * @return array
+	 */
+	private function get_no_album_art_image_array(){
+		$src    = SSP_PLUGIN_URL . '/assets/images/no-album-art.png';
+		$width  = 300;
+		$height = 300;
+		
+		return compact( 'src', 'width', 'height' );
+	}
+	
 	/**
 	 * Load media player for given file
 	 * @param  string  $srcFile        Source of file
@@ -419,21 +468,7 @@ class SSP_Frontend {
 						// ---- NEW PLAYER -----
 
 						// Get episode album art
-						$thumb_id = get_post_thumbnail_id( $episode_id );
-						
-						if ( ! empty( $thumb_id ) ) {
-							
-							list( $src, $width, $height ) = wp_get_attachment_image_src( $thumb_id, 'full' );
-							
-							$albumArt = compact( 'src', 'width', 'height' );
-							
-							if ( ( $width / $height ) !== 1 ) {
-								$albumArt = $this->get_album_art( $episode_id );
-							}
-							
-						}else {
-							$albumArt = $this->get_album_art( $episode_id );
-						}
+						$albumArt = $this->get_album_art( $episode_id );
 
 						$player_background_colour = get_option( 'ss_podcasting_player_background_skin_colour', false );
 						$player_wave_form_colour = get_option( 'ss_podcasting_player_wave_form_colour', false );

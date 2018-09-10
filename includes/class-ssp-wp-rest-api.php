87516a285a39f5dbb1ddfd5d33ae0083fd586496
Jonathan Bossenger <jonathanbossenger@gmail.com>
2018-09-10 16:47:22 +0200

    Adding Series REST API fields

--- includes/class-ssp-wp-rest-api.php
+++ includes/class-ssp-wp-rest-api.php
@@ -14,6 +14,34 @@ class SSP_WP_REST_API {
 	private $version;
 
 	/**
+	 * Gets the default podcast data
+	 *
+	 * @return array Podcast
+	 */
+	private function get_default_podcast_settings() {
+
+		$podcast = array();
+
+		$podcast['title']           = get_option( 'ss_podcasting_data_title', get_bloginfo( 'name' ) );
+		$description                = get_option( 'ss_podcasting_data_description', get_bloginfo( 'description' ) );
+		$podcast['description']     = mb_substr( strip_tags( $description ), 0, 3999 );
+		$podcast['language']        = get_option( 'ss_podcasting_data_language', get_bloginfo( 'language' ) );
+		$podcast['copyright']       = get_option( 'ss_podcasting_data_copyright', '&#xA9; ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) );
+		$podcast['subtitle']        = get_option( 'ss_podcasting_data_subtitle', get_bloginfo( 'description' ) );
+		$podcast['author']          = get_option( 'ss_podcasting_data_author', get_bloginfo( 'name' ) );
+		$podcast['owner_name']      = get_option( 'ss_podcasting_data_owner_name', get_bloginfo( 'name' ) );
+		$podcast['owner_email']     = get_option( 'ss_podcasting_data_owner_email', get_bloginfo( 'admin_email' ) );
+		$podcast['explicit_option'] = get_option( 'ss_podcasting_explicit', '' );
+		$podcast['complete_option'] = get_option( 'ss_podcasting_complete', '' );
+		$podcast['image']           = get_option( 'ss_podcasting_data_image', '' );
+		$podcast['category1']       = ssp_get_feed_category_output( 1 );
+		$podcast['category2']       = ssp_get_feed_category_output( 2 );
+		$podcast['category3']       = ssp_get_feed_category_output( 3 );
+
+		return $podcast;
+	}
+
+	/**
 	 * Constructor
 	 *
 	 * @param    string $version Plugin version
@@ -23,6 +51,8 @@ class SSP_WP_REST_API {
 
 		// Register custom REST API routes.
 		add_action( 'rest_api_init', array( $this, 'register_rest_routes' ) );
+
+		add_action( 'rest_api_init', array( $this, 'create_api_series_fields' ) );
 
 	}
 
@@ -37,31 +67,41 @@ class SSP_WP_REST_API {
 	}
 
 	/**
-	 * Gets the default podcast data
+	 * Gets the podcast data for the podcast route
 	 *
-	 * @return array Podcast
+	 * @return array $podcast Podcast data
 	 */
-	public function get_rest_podcast() {
-
-		$podcast = array();
-
-		$podcast['title']               = get_option( 'ss_podcasting_data_title', get_bloginfo( 'name' ) );
-		$description                    = get_option( 'ss_podcasting_data_description', get_bloginfo( 'description' ) );
-		$podcast['podcast_description'] = mb_substr( strip_tags( $description ), 0, 3999 );
-		$podcast['language']            = get_option( 'ss_podcasting_data_language', get_bloginfo( 'language' ) );
-		$podcast['copyright']           = get_option( 'ss_podcasting_data_copyright', '&#xA9; ' . date( 'Y' ) . ' ' . get_bloginfo( 'name' ) );
-		$podcast['subtitle']            = get_option( 'ss_podcasting_data_subtitle', get_bloginfo( 'description' ) );
-		$podcast['author']              = get_option( 'ss_podcasting_data_author', get_bloginfo( 'name' ) );
-		$podcast['owner_name']          = get_option( 'ss_podcasting_data_owner_name', get_bloginfo( 'name' ) );
-		$podcast['owner_email']         = get_option( 'ss_podcasting_data_owner_email', get_bloginfo( 'admin_email' ) );
-		$podcast['explicit_option']     = get_option( 'ss_podcasting_explicit', '' );
-		$podcast['complete_option']     = get_option( 'ss_podcasting_complete', '' );
-		$podcast['image']               = get_option( 'ss_podcasting_data_image', '' );
-		$podcast['category1']           = ssp_get_feed_category_output( 1 );
-		$podcast['category2']           = ssp_get_feed_category_output( 2 );
-		$podcast['category3']           = ssp_get_feed_category_output( 3 );
-
+	public function get_rest_podcast(){
+		$podcast = $this->get_default_podcast_settings();
 		return $podcast;
+	}
+
+
+	public function create_api_series_fields() {
+		$podcast_fields = array_keys( $this->get_default_podcast_settings() );
+		// register_rest_field ( 'name-of-post-type', 'name-of-field-to-return', array-of-callbacks-and-schema() )
+		foreach ( $podcast_fields as $podcast_field ) {
+			register_rest_field( 'series', $podcast_field, array(
+					'get_callback' => array( $this, 'series_get_field_value' ),
+				)
+			);
+
+		}
+	}
+
+	public function series_get_field_value( $data, $field_name, $request ) {
+		//print_r($data);
+		//print_r($field_name);
+		//wp_die('here');
+		$podcast            = $this->get_default_podcast_settings();
+		$field_value        = $podcast[ $field_name ];
+		$series_id          = $data['id'];
+		$series_field_value = get_option( 'ss_podcasting_data_' . $field_name . '_' . $series_id, '' );
+		if ( $series_field_value ) {
+			$field_value = $series_field_value;
+		}
+
+		return $field_value;
 	}
 
 }

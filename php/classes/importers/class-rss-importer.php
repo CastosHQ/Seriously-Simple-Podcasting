8006fea5c34cee50bda7de5c5ffe32513d2b0a02
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-03-02 11:10:52 +0200

    Update the importer to check for the correct post_content values

--- php/classes/importers/class-rss-importer.php
+++ php/classes/importers/class-rss-importer.php
@@ -81,7 +81,7 @@ class Rss_Importer {
 	 * Load the xml feed url into the feed_object
 	 */
 	public function load_rss_feed() {
-		$this->feed_object = simplexml_load_file( $this->rss_feed );
+		$this->feed_object = simplexml_load_string( file_get_contents( $this->rss_feed ) );
 	}
 
 	/**
@@ -90,6 +90,49 @@ class Rss_Importer {
 	public function update_ssp_rss_import() {
 		$progress = round( ( $this->podcast_added / $this->podcast_count ) * 100 );
 		update_option( 'ssp_rss_import', $progress );
+	}
+
+	/**
+	 * Get the value for post_content from the RSS episode item
+	 *
+	 * @param $item
+	 *
+	 * @return string
+	 */
+	public function get_item_post_content( $item, $itunes ) {
+		$content = $item->children( 'content', true );
+		if ( ! empty( $content->encoded ) ) {
+			return trim( (string) $content->encoded );
+		}
+		if ( ! empty( $item->description ) ) {
+			return trim( (string) $item->description );
+		}
+		if ( ! empty( $itunes->summary ) ) {
+			return trim( (string) $itunes->summary );
+		}
+
+		return '';
+	}
+
+	/**
+	 * Create post_data from RSS Feed item
+	 *
+	 * @param $item
+	 *
+	 * @return array
+	 */
+	public function get_post_data( $item ) {
+		$itunes                    = $item->children( 'http://www.itunes.com/dtds/podcast-1.0.dtd' );
+		$post_data                 = array();
+		$post_data['post_content'] = $this->get_item_post_content( $item, $itunes );
+		$post_data['post_excerpt'] = trim( (string) $itunes->subtitle );
+		$post_data['post_title']   = trim( (string) $item->title );
+		$post_data['post_status']  = 'publish';
+		$post_data['post_author']  = get_current_user_id();
+		$post_data['post_date']    = date( 'Y-m-d H:i:s', strtotime( (string) $item->pubDate ) ); //phpcs:ignore WordPress.NamingConventions
+		$post_data['post_type']    = $this->post_type;
+
+		return $post_data;
 	}
 
 	/**
@@ -106,23 +149,7 @@ class Rss_Importer {
 		for ( $i = 0; $i < $this->podcast_count; $i ++ ) {
 
 			$item = $this->feed_object->channel->item[ $i ];
-
-			$itunes      = $item->children( 'http://www.itunes.com/dtds/podcast-1.0.dtd' );
-			$post_title  = trim( (string) $item->title );
-			$post_author = get_current_user_id();
-			$url         = (string) $item->enclosure['url'];
-			$post_date   = strtotime( (string) $item->pubDate ); //phpcs:ignore WordPress.NamingConventions
-
-			// Setup the post array.
-			$post = array(
-				'post_content' => trim( (string) $itunes->summary ),
-				'post_excerpt' => trim( (string) $itunes->subtitle ),
-				'post_title'   => $post_title,
-				'post_status'  => 'publish',
-				'post_author'  => $post_author,
-				'post_date'    => date( 'Y-m-d H:i:s', $post_date ),
-				'post_type'    => $this->post_type,
-			);
+			$post = $this->get_post_data( $item );
 
 			// Add the post
 			$post_id = wp_insert_post( $post );
@@ -134,6 +161,7 @@ class Rss_Importer {
 				continue;
 			}
 
+			$url = (string) $item->enclosure['url'];
 			// strips out any possible weirdness in the file url
 			$url = preg_replace( '/(?s:.*)(https?:\/\/(?:[\w\-\.]+[^#?\s]+)(?:\.mp3))(?s:.*)/', '$1', $url );
 
@@ -147,7 +175,7 @@ class Rss_Importer {
 
 			// Update the added count and imported title array
 			$this->podcast_added ++;
-			$this->podcasts_imported[] = $post_title;
+			$this->podcasts_imported[] = $post['post_title'];
 
 			$this->update_ssp_rss_import();
 		}

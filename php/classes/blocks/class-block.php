8fc3584bc9ed759b49496c2b8555dc6bad15de49
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-04-24 14:53:29 +0200

    Tutorial updates
    Registering a block using register_block_type
    Preparing external assets for loading

--- php/classes/blocks/class-block.php
+++ php/classes/blocks/class-block.php
@@ -27,7 +27,9 @@ class Block extends Controller {
 
 	protected function bootstrap() {
 		$this->asset_file = include SSP_PLUGIN_PATH . '/build/index.asset.php';
-		add_action( 'enqueue_block_editor_assets', array( $this, 'block_enqueue_scripts' ) );
+		add_action( 'init', array( $this, 'examples_01_register_block' ) );
+		//add_action( 'enqueue_block_editor_assets', array( $this, 'block_enqueue_scripts' ) );
+		//add_action( 'enqueue_block_assets', 'block_enqueue_styles' );
 	}
 
 	public function block_enqueue_scripts() {
@@ -39,6 +41,27 @@ class Block extends Controller {
 			true
 		);
 		wp_enqueue_script( 'ssp-block-script' );
+	}
+
+	public function block_enqueue_styles() {
+		//
+	}
+
+	public function examples_01_register_block() {
+		wp_register_script(
+			'ssp-block-script',
+			esc_url( SSP_PLUGIN_URL . 'build/index.js' ),
+			$this->asset_file['dependencies'],
+			$this->asset_file['version'],
+			true
+		);
+
+		register_block_type(
+			'seriously-simple-podcasting/example-01-basic-esnext',
+			array(
+				'editor_script' => 'ssp-block-script',
+			)
+		);
 	}
 
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,48 @@
 import { registerBlockType } from '@wordpress/blocks';
 
+/**
+ * Example 1
+ * @type {{padding: string, backgroundColor: string, color: string}}
+ */
+const blockStyle = {
+	backgroundColor: '#900',
+	color: '#fff',
+	padding: '20px',
+};
+
+registerBlockType( 'seriously-simple-podcasting/example-01-basic-esnext', {
+	title: 'Example: Basic (esnext)',
+	icon: 'universal-access-alt',
+	category: 'layout',
+	example: {},
+	edit() {
+		return <div style={ blockStyle }>Hello World, step 1 (from the editor).</div>;
+	},
+	save() {
+		return <div style={ blockStyle }>Hello World, step 1 (from the frontend).</div>;
+	},
+} );
+
+/**
+ * Example 2
+
+registerBlockType( 'seriously-simple-podcasting/example-02-stylesheets', {
+	title: 'Example: Stylesheets',
+	icon: 'universal-access-alt',
+	category: 'layout',
+	example: {},
+	edit( { className } ) {
+		return <p className={ className }>Hello World, step 2 (from the editor, in green).</p>;
+	},
+	save() {
+		return <p>Hello World, step 2 (from the frontend, in red).</p>;
+	},
+} );
+
 registerBlockType(
 	'seriously-simple-podcasting/player-block', {
 		title: 'Castos Player',
-		icon: 'smiley',
+		icon: 'controls-volumeoff',
 		category: 'layout',
 		edit: () => <iframe className="castos-iframe-player" src="https://wp-hacker-cast.castos.com/player/150332" frameBorder="0" scrolling="no" width="100%" height="150"></iframe>,
 		save: () => <iframe className="castos-iframe-player" src="https://wp-hacker-cast.castos.com/player/150332" frameBorder="0" scrolling="no" width="100%" height="150"></iframe>,
@@ -23,3 +62,4 @@ registerBlockType(
 		,
 	}
 );
+*/

81621e23bfb723f09057f83a65e77d45098d8d07
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-05-06 16:34:34 +0200

    Removing wave objects from CastosPlayer block
    These are automatically rendered on the front end.

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => '6939003a2997cc59ccfc75c3ff3eb2b3');
+<?php return array('dependencies' => array('wp-blocks', 'wp-element', 'wp-polyfill'), 'version' => 'b69852088d18597282a78876ed45125b');

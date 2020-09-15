d5586f56015b5e541cf52bcdd99894ee9d20d427
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-09-15 10:24:13 +0200

    Update class-players-controller.php

--- php/classes/controllers/class-players-controller.php
+++ php/classes/controllers/class-players-controller.php
@@ -24,6 +24,11 @@ class Players_Controller extends Controller {
 	public function __construct( $file, $version ) {
 		parent::__construct( $file, $version );
 		$this->render = new Renderer();
+		add_shortcode('elementor_html_player', array($this, 'elementor_html_player'));
+	}
+	
+	public function elementor_html_player($atttributes){
+		return $this->html_player($atttributes['id'])
 	}
 
 	public function html_player($id){

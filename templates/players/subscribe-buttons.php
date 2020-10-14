556629a618f39e7cf96d1573333bfb9f519b2020
Danilo Radovic <daniloradovic@Danilos-MacBook-Pro.local>
2020-10-14 21:31:05 +0200

    changes on subscribe buttons widget - select podcast/series and re-render subscribe buttons

--- templates/players/subscribe-buttons.php
+++ templates/players/subscribe-buttons.php
@@ -2,10 +2,12 @@
 	Subscribe:
 	<div class="ssp-subscribe-buttons">
 		<?php foreach ( $subscribe_urls as $subscribe_url ) { ?>
-			<a href="<?php echo $subscribe_url['url'] ?>" target="_blank" rel="noopener noreferrer">
-				<img src="<?php echo SSP_PLUGIN_URL ?>/assets/icons/subscribe/<?php echo $subscribe_url['icon'] ?>">
-				<span style=""><?php echo $subscribe_url['label'] ?></span>
-			</a>
+            <?php if(!empty( $subscribe_url['url'])): ?>
+                <a href="<?php echo $subscribe_url['url'] ?>" target="_blank" rel="noopener noreferrer">
+                    <img src="<?php echo SSP_PLUGIN_URL ?>/assets/icons/subscribe/<?php echo $subscribe_url['icon'] ?>">
+                    <span style=""><?php echo $subscribe_url['label'] ?></span>
+                </a>
+			<?php endif; ?>
 		<?php } ?>
 	</div>
 </div>

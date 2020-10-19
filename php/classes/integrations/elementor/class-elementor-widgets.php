d670703c346fac4aa6af798cb31b4fd4f3c80e56
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-10-19 08:41:55 +0200

    Add Elementor specific checks to the Elementor_Widgets class

--- php/classes/integrations/elementor/class-elementor-widgets.php
+++ php/classes/integrations/elementor/class-elementor-widgets.php
@@ -10,15 +10,55 @@ use SeriouslySimplePodcasting\Integrations\Elementor\Widgets\Elementor_Subscribe
 
 final class Elementor_Widgets {
 
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 2.4
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 2.4
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
+
 	public function __construct() {
-		$this->init();
+		add_action( 'plugins_loaded', [ $this, 'on_plugins_loaded' ] );
+	}
+
+	public function on_plugins_loaded() {
+		if ( $this->is_compatible() ) {
+			add_action( 'elementor/init', [ $this, 'init' ] );
+		}
+	}
+
+	public function is_compatible() {
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			return false;
+		}
+		// Check for required Elementor version
+		if ( ! version_compare( ELEMENTOR_VERSION, $this::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			return false;
+		}
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, $this::MINIMUM_PHP_VERSION, '<' ) ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	public function init() {
 		add_action( 'elementor/elements/categories_registered', [ $this, 'add_elementor_widget_categories' ] );
 		add_action( 'elementor/widgets/widgets_registered', [ $this, 'init_widgets' ] );
 	}
-
 
 	public function add_elementor_widget_categories( $elements_manager ) {
 		$elements_manager->add_category(

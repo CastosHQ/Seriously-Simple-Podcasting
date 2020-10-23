502694da087218b3c1d2c2fd7a519adc9e284c7a
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-10-23 14:38:35 +0200

    Updating Extensions list

--- php/classes/controllers/class-extensions-controller.php
+++ php/classes/controllers/class-extensions-controller.php
@@ -120,7 +120,7 @@ class Extensions_Controller extends Controller {
 		if ( ssp_is_elementor_ok() ) {
 			$elementor_templates = array(
 				'title'       => __( 'Elementor Templates', 'seriously-simple-podcasting' ),
-				'image'       => $image_dir . 'elementor.png',
+				'image'       => $image_dir . 'elementor.jpg',
 				'url'         => wp_nonce_url( admin_url( 'edit.php?post_type=podcast&page=podcast_settings&tab=extensions&elementor_import_templates=true' ), '', 'import_template_nonce' ),
 				'description' => __( 'Looking for a custom elementor template to use with Seriously Simple Podcasting? Click here to import all of them righ now!', 'seriously-simple-podcasting' ),
 				'button_text' => __( 'Import Templates', 'seriously-simple-podcasting' ),

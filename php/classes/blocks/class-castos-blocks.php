15873e40e98d69d57ffc1780a73a0e0b2023e04e
Jonathan Bossenger <jonathanbossenger@gmail.com>
2020-11-18 15:52:17 +0200

    Blocks registration clean up

--- php/classes/blocks/class-castos-blocks.php
+++ php/classes/blocks/class-castos-blocks.php
@@ -25,25 +25,40 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 class Castos_Blocks extends Controller {
 
+	/**
+	 * @var Blocks asset file
+	 */
 	protected $asset_file;
 
+	/**
+	 * Castos_Blocks constructor.
+	 *
+	 * @param $file
+	 * @param $version
+	 */
 	public function __construct( $file, $version ) {
 		parent::__construct( $file, $version );
 		$this->bootstrap();
 	}
 
+	/**
+	 * Dynamic Podcast List Block callback
+	 *
+	 * @param $attributes
+	 *
+	 * @return string
+	 */
 	public function podcast_list_render_callback($attributes) {
 		global $ss_podcasting;
 		return $ss_podcasting->render_podcast_list_dynamic_block($attributes);
 	}
 
+	/**
+	 * Loads the asset file and the block registration
+	 */
 	protected function bootstrap() {
 		$this->asset_file = include SSP_PLUGIN_PATH . 'build/index.asset.php';
-		// register the actual blocks
 		add_action( 'init', array( $this, 'register_castos_blocks' ) );
-		// enqueue our plugin assets for the block editor and rendering the block
-		//add_action( 'enqueue_block_editor_assets', array( $this, 'enqueue_block_editor_assets' ) );
-		//add_action( 'enqueue_block_assets', array( $this, 'enqueue_player_assets' ) );
 	}
 
 	/**
@@ -71,16 +86,26 @@ class Castos_Blocks extends Controller {
 
 		wp_register_style(
 			'ssp-castos-player',
-			esc_url( SSP_PLUGIN_URL . 'assets/css/castos-player-v1.css' ),
+			esc_url( SSP_PLUGIN_URL . 'assets/css/castos-player.css' ),
 			array(),
 			$this->asset_file['version']
 		);
+
+		/*register_block_type( 'seriously-simple-podcasting/block-name',
+			array(
+				'editor_script' => '',
+				'editor_style'  => '',
+				'script'        => '',
+				'style'         => '',
+			)
+		);*/
 
 		register_block_type(
 			'seriously-simple-podcasting/castos-player',
 			array(
 				'editor_script' => 'ssp-block-script',
 				'editor_style'  => 'ssp-castos-player',
+				'style'  => 'ssp-castos-player',
 			)
 		);
 
@@ -97,119 +122,5 @@ class Castos_Blocks extends Controller {
 				'render_callback' => array( $this, 'podcast_list_render_callback' )
 			)
 		);
-
-		/*register_block_type( 'seriously-simple-podcasting/block-name',
-			array(
-				'editor_script' => '',
-				'editor_style'  => '',
-				'script'        => '',
-				'style'         => '',
-			)
-		);*/
-
 	}
-
-	/**
-	 * Enqueues all front end assets needed to render the Castos player correctly
-	 */
-	/*public function enqueue_block_editor_assets(){
-		if ( defined( 'SCRIPT_DEBUG' ) ) {
-			$wavesurfer_src = '//cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.4.0/wavesurfer.js';
-		} else {
-			$wavesurfer_src = '//cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.4.0/wavesurfer.min.js';
-		}
-		wp_register_script(
-			'ssp-block-wavesurfer',
-			$wavesurfer_src,
-			array(),
-			$this->asset_file['version'],
-			true
-		);
-
-		wp_register_script(
-			'ssp-block-media-player',
-			esc_url( SSP_PLUGIN_URL . 'assets/js/media.player.js' ),
-			array( 'jquery' ),
-			$this->asset_file['version'],
-			true
-		);
-
-		wp_register_script(
-			'ssp-block-html5-player',
-			esc_url( SSP_PLUGIN_URL . 'assets/js/html5.player.js' ),
-			array( 'jquery' ),
-			$this->asset_file['version'],
-			true
-		);
-
-		wp_register_style(
-			'ssp-block-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/css/block_style.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-		wp_register_style(
-			'ssp-block-fonts-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/css/icon_fonts.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-		wp_register_style(
-			'ssp-block-gizmo-fonts-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/fonts/Gizmo/gizmo.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-
-		wp_enqueue_script( 'ssp-block-wavesurfer' );
-		wp_enqueue_script( 'ssp-block-media-player' );
-		wp_enqueue_script( 'ssp-block-html5-player' );
-
-		wp_enqueue_style( 'ssp-block-style' );
-		wp_enqueue_style( 'ssp-block-fonts-style' );
-		wp_enqueue_style( 'ssp-block-gizmo-fonts-style' );
-	}*/
-
-	/**
-	 * Enqueues SSP plugin assets needed for the player to work.
-	 */
-	/*public function enqueue_player_assets() {
-		if ( defined( 'SCRIPT_DEBUG' ) ) {
-			$wavesurfer_src = '//cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.4.0/wavesurfer.js';
-		} else {
-			$wavesurfer_src = '//cdnjs.cloudflare.com/ajax/libs/wavesurfer.js/1.4.0/wavesurfer.min.js';
-		}
-		wp_register_script(
-			'ssp-block-wavesurfer',
-			$wavesurfer_src,
-			array(),
-			$this->asset_file['version'],
-			true
-		);
-
-		wp_register_style(
-			'ssp-block-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/css/block_style.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-		wp_register_style(
-			'ssp-block-fonts-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/css/icon_fonts.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-		wp_register_style(
-			'ssp-block-gizmo-fonts-style',
-			esc_url( SSP_PLUGIN_URL . 'assets/fonts/Gizmo/gizmo.css' ),
-			array(),
-			$this->asset_file['version']
-		);
-
-		wp_enqueue_script( 'ssp-block-wavesurfer' );
-
-		wp_enqueue_style( 'ssp-block-style' );
-		wp_enqueue_style( 'ssp-block-fonts-style' );
-		wp_enqueue_style( 'ssp-block-gizmo-fonts-style' );
-	}*/
 }

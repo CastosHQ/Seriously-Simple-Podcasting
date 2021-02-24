9cb5898872b3b484d4337ab692ba73ba4ac272fa
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2021-02-24 14:51:51 +0200

    Revert "Change right sidebar in settings"
    This reverts commit f2e2a0dc514c2b04d29eb535f41bba8b9e21481c.

--- templates/settings-sidebar.php
+++ templates/settings-sidebar.php
@@ -1,10 +1,29 @@
 <div id="ssp-sidebar">
 	<div class="sidebar-content">
-		<h2>Join Castos Academy</h2>
-		<p>Enhance your podcast, grow your audience, and monetize your shows through our free podcasting academy.</p>
-		<a href="https://academy.castos.com/?utm_source=plugin&utm_medium=banner&utm_campaign=sidebar">
-			Join For Free Today
-		</a>
-		<img src="<?php echo $image_dir ?>join-castos-academy.png">
+		<img src="<?php echo $image_dir ?>castos-plugin-settings-banner.jpg" src="https://www.castos.com">
+		<form action="https://www.getdrip.com/forms/38739479/submissions" method="post" data-drip-embedded-form="38739479">
+			<h3 data-drip-attribute="headline">Castos Hosting Discount - Get 20% off</h3>
+			<div data-drip-attribute="description">Drop in your name and email and we’ll send you a coupon for 20% off your subscription to Castos Podcast Hosting.</div>
+			<div>
+				<label for="drip-first-name">First Name</label><br />
+				<input type="text" id="drip-first-name" name="fields[first_name]" value="" />
+			</div>
+			<div>
+				<label for="drip-last-name">Last Name</label><br />
+				<input type="text" id="drip-last-name" name="fields[last_name]" value="" />
+			</div>
+			<div>
+				<label for="drip-email">Email Address</label><br />
+				<input type="email" id="drip-email" name="fields[email]" value="" />
+			</div>
+			<div style="display: none;" aria-hidden="true">
+				<label for="website">Website</label><br />
+				<input type="text" id="website" name="website" tabindex="-1" autocomplete="false" value="" />
+			</div>
+			<div>
+				<input type="submit" value="Send me the coupon" data-drip-attribute="sign-up-button" />
+			</div>
+			<p>Spam sucks. We will not use your email for anything else and you can unsubscribe with just a click, anytime.</p>
+		</form>
 	</div>
 </div>

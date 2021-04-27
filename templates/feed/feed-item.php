54384899a64e7ef77b04fcfe755c70812f183823
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2021-04-27 22:34:01 +0300

    Excerpt for content:encoded tag
    Issue  #676

--- templates/feed/feed-item.php
+++ templates/feed/feed-item.php
@@ -232,7 +232,7 @@ $title = esc_html( get_the_title_rss() );
 		<itunes:season><?php echo $itunes_season_number; ?></itunes:season>
 	<?php endif; ?>
 	<?php if ( ! isset( $turbo_post_count ) || $turbo_post_count <= 10 ) { ?>
-		<content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
+		<content:encoded><![CDATA[<?php echo $description; ?>]]></content:encoded>
 	<?php } ?>
 	<enclosure url="<?php echo esc_url( $enclosure ); ?>" length="<?php echo esc_attr( $size ); ?>"
 			   type="<?php echo esc_attr( $mime_type ); ?>"></enclosure>

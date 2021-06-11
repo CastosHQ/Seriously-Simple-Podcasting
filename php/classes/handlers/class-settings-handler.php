b53909db632f285bf76cded5cffe80425544a67d
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2021-06-11 15:44:45 +0300

    Revert "Default option to exclude series from default feed"
    This reverts commit 98c1edc476653288b36d26afb49cdbab9fac48b1.

--- php/classes/handlers/class-settings-handler.php
+++ php/classes/handlers/class-settings-handler.php
@@ -829,7 +829,7 @@ class Settings_Handler {
 				// translators: placeholders are html anchor tags to support document
 				'description' => sprintf( __( 'When enabled, this will exclude any episodes in this series feed from the default feed. %1$sMore details here.%2$s', 'seriously-simple-podcasting' ), '<a href="' . esc_url( 'https://support.castos.com/article/114-excluding-series-episodes-from-the-default-feed' ) . '" target="' . wp_strip_all_tags( '_blank' ) . '">', '</a>' ),
 				'type'        => 'checkbox',
-				'default'     => 'on',
+				'default'     => '',
 				'callback'    => 'wp_strip_all_tags',
 			),
 			array(

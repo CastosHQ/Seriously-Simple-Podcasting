ecf14e043291a8d0b1361632b0a5ef881869f276
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2021-08-03 17:32:19 +0300

    Fixed conflict between SSP and The Events Calendar Plugin (#754)

--- php/classes/controllers/class-frontend-controller.php
+++ php/classes/controllers/class-frontend-controller.php
@@ -890,9 +890,9 @@ class Frontend_Controller extends Controller {
 			return;
 		}
 
-		$tag_archive_post_types = apply_filters( 'ssp_tag_archive_post_types', array('post', SSP_CPT_PODCAST) ) ;
-		$query->set( 'post_type', $tag_archive_post_types );
-
+		$post_types             = $query->get( 'post_type' );
+		$tag_archive_post_types = apply_filters( 'ssp_tag_archive_post_types', array( 'post', SSP_CPT_PODCAST ) );
+		$query->set( 'post_type', array_merge( $post_types, $tag_archive_post_types ) );
 	}
 
 	/**

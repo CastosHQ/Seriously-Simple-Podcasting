a312f8efd1ff03243a56e8eabb5028e5d2305403
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2021-09-14 21:39:17 +0300

    Author feed tag redundant > character fix #781

--- templates/feed-podcast.php
+++ templates/feed-podcast.php
@@ -77,7 +77,7 @@ if ( $stylesheet_url ) {
 		<language><?php echo esc_html( $language ); ?></language>
 		<copyright><?php echo esc_html( $copyright ); ?></copyright>
 		<itunes:subtitle><?php echo esc_html( $subtitle ); ?></itunes:subtitle>
-		<itunes:author><?php echo esc_html( $author ); ?>></itunes:author>
+		<itunes:author><?php echo esc_html( $author ); ?></itunes:author>
 		<?php if ( $itunes_type ) : ?>
 			<itunes:type><?php echo $itunes_type; ?></itunes:type>
 		<?php endif ?>

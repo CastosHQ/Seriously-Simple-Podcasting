bb1b61d45f44dc2b87eea6703ffa6136ed98f2a3
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2022-04-13 14:58:56 +0300

    Version 2.13.0-beta.2

--- seriously-simple-podcasting.php
+++ seriously-simple-podcasting.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Seriously Simple Podcasting
- * Version: 2.13.0-beta
+ * Version: 2.13.0-beta.2
  * Plugin URI: https://castos.com/seriously-simple-podcasting/?utm_medium=sspodcasting&utm_source=wordpress&utm_campaign=wpplugin_08_2019
  * Description: Podcasting the way it's meant to be. No mess, no fuss - just you and your content taking over the world.
  * Author: Castos
@@ -25,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 use SeriouslySimplePodcasting\Controllers\App_Controller;
 
-define( 'SSP_VERSION', '2.13.0-beta' );
+define( 'SSP_VERSION', '2.13.0-beta.2' );
 define( 'SSP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SSP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 

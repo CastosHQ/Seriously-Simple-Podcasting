3b04b42bd99329aba34cc55a70e7483418023cd0
Sergey Zakharchenko <zaharchenko.dev@gmail.com>
2022-04-25 20:51:23 +0300

    Feed: podcast:transcript #758

--- templates/feed/feed-item.php
+++ templates/feed/feed-item.php
@@ -23,6 +23,8 @@
  * @var string $duration
  * @var string $gp_description
  * @var string $googleplay_explicit_flag
+ * @var string $transcript_url Added with SSTranscript plugin
+ * @var string $transcript_mime Added with SSTranscript plugin
  */
 ?>
 
@@ -34,47 +36,70 @@
 	<guid isPermaLink="false"><?php the_guid(); ?></guid>
 	<description><![CDATA[<?php echo $description; ?>]]></description>
 	<itunes:subtitle><![CDATA[<?php echo $itunes_subtitle; ?>]]></itunes:subtitle>
-	<?php if ( $keywords ) : ?>
-		<itunes:keywords><?php echo $keywords; ?></itunes:keywords>
-	<?php endif; ?>
-	<?php if ( $itunes_episode_type ) : ?>
-		<itunes:episodeType><?php echo $itunes_episode_type; ?></itunes:episodeType>
-	<?php endif; ?>
-	<?php if ( $itunes_title ): ?>
-		<itunes:title><![CDATA[<?php echo $itunes_title; ?>]]></itunes:title>
-	<?php endif; ?>
-	<?php if ( $itunes_episode_number ): ?>
-		<itunes:episode><?php echo $itunes_episode_number; ?></itunes:episode>
-	<?php endif; ?>
-	<?php if ( $itunes_season_number ): ?>
-		<itunes:season><?php echo $itunes_season_number; ?></itunes:season>
-	<?php endif; ?>
-	<?php if ( ! isset( $turbo_post_count ) || $turbo_post_count <= 10 ) { ?>
-		<content:encoded><![CDATA[<?php echo $description; ?>]]></content:encoded>
-	<?php } ?>
-	<enclosure url="<?php echo esc_url( $enclosure ); ?>" length="<?php echo esc_attr( $size ); ?>"
-			   type="<?php echo esc_attr( $mime_type ); ?>"></enclosure>
-	<?php if ( ! isset( $turbo_post_count ) || $turbo_post_count <= 10 ) { ?>
-		<itunes:summary><![CDATA[<?php echo $itunes_summary; ?>]]></itunes:summary>
-	<?php } ?>
-	<?php if ( $episode_image ) { ?>
-		<itunes:image href="<?php echo esc_url( $episode_image ); ?>"></itunes:image>
-		<image>
-			<url><?php echo esc_url( $episode_image ); ?></url>
-			<title><?php echo esc_attr( $title ); ?></title>
-		</image>
-	<?php } ?>
+<?php
+
+	if ( $keywords ) : ?>
+	<itunes:keywords><?php echo $keywords; ?></itunes:keywords>
+<?php endif;
+
+if ( $itunes_episode_type ) : ?>
+	<itunes:episodeType><?php echo $itunes_episode_type; ?></itunes:episodeType>
+<?php endif;
+
+if ( $itunes_title ): ?>
+	<itunes:title><![CDATA[<?php echo $itunes_title; ?>]]></itunes:title>
+<?php endif;
+
+if ( $itunes_episode_number ): ?>
+	<itunes:episode><?php echo $itunes_episode_number; ?></itunes:episode>
+<?php endif;
+
+if ( $itunes_season_number ): ?>
+	<itunes:season><?php echo $itunes_season_number; ?></itunes:season>
+<?php endif;
+
+if ( ! isset( $turbo_post_count ) || $turbo_post_count <= 10 ) : ?>
+	<content:encoded><![CDATA[<?php echo $description; ?>]]></content:encoded>
+<?php endif;
+
+?>
+	<enclosure url="<?php echo esc_url( $enclosure ); ?>" length="<?php echo esc_attr( $size ); ?>" type="<?php echo esc_attr( $mime_type ); ?>"></enclosure>
+<?php
+
+if ( ! isset( $turbo_post_count ) || $turbo_post_count <= 10 ) : ?>
+	<itunes:summary><![CDATA[<?php echo $itunes_summary; ?>]]></itunes:summary>
+<?php endif;
+
+if ( $episode_image ) : ?>
+	<itunes:image href="<?php echo esc_url( $episode_image ); ?>"></itunes:image>
+	<image>
+		<url><?php echo esc_url( $episode_image ); ?></url>
+		<title><?php echo esc_attr( $title ); ?></title>
+	</image>
+<?php endif;
+
+?>
 	<itunes:explicit><?php echo esc_html( $itunes_explicit_flag ); ?></itunes:explicit>
 	<itunes:block><?php echo esc_html( $block_flag ); ?></itunes:block>
 	<itunes:duration><?php echo esc_html( $duration ); ?></itunes:duration>
 	<itunes:author><![CDATA[<?php echo $author; ?>]]></itunes:author><?php
-	 if ( ! $turbo_post_count ) { ?>
-		<googleplay:description><![CDATA[<?php echo $gp_description; ?>]]></googleplay:description>
-		<?php if ( $episode_image ) { ?>
-			<googleplay:image href="<?php echo esc_url( $episode_image ); ?>"></googleplay:image>
-		<?php } ?>
-		<googleplay:explicit><?php echo esc_html( $googleplay_explicit_flag ); ?></googleplay:explicit>
-		<googleplay:block><?php echo esc_html( $block_flag ); ?></googleplay:block><?php
-	 } ?>
 
+if ( ! $turbo_post_count ) : ?>
+	<googleplay:description><![CDATA[<?php echo $gp_description; ?>]]></googleplay:description>
+<?php endif;
+
+if ( $episode_image ) : ?>
+	<googleplay:image href="<?php echo esc_url( $episode_image ); ?>"></googleplay:image>
+<?php endif;
+
+?>
+	<googleplay:explicit><?php echo esc_html( $googleplay_explicit_flag ); ?></googleplay:explicit>
+	<googleplay:block><?php echo esc_html( $block_flag ); ?></googleplay:block>
+<?php
+
+if ( $transcript_url && $transcript_mime ) : ?>
+	<podcast:transcript url="<?php echo esc_url( $transcript_url ) ?>" type="<?php echo esc_attr( $transcript_mime ) ?>"/>
+<?php endif;
+
+?>
 </item>

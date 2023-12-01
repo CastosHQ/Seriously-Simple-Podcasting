3a4eaa095aae76978ae1be2851e537e2589ff806
Sergiy Zakharchenko <zaharchenko.dev@gmail.com>
2023-12-01 13:20:43 +0000

    Fixed the Transcripts plugin support #1071

--- templates/metafields/file.php
+++ templates/metafields/file.php
@@ -10,7 +10,7 @@
   echo wp_kses_post( $v['name'] ) ?></label></p>
 <p><input name="<?php echo esc_attr( $k ) ?>" type="text" id="upload_<?php
   echo esc_attr( $k ) ?>" value="<?php echo esc_attr( $data ) ?>" />
-<input type="button" class="button" id="upload_'<?php echo esc_attr( $k ) ?>_button" value="<?php
+<input type="button" class="button" id="upload_<?php echo esc_attr( $k ) ?>_button" value="<?php
 	_e( 'Upload File', 'seriously-simple-podcasting' )
 	?>" data-uploader_title="<?php _e( 'Choose a file', 'seriously-simple-podcasting' )
 	?>" data-uploader_button_text="<?php _e( 'Insert podcast file', 'seriously-simple-podcasting' ) ?>" /><br/>

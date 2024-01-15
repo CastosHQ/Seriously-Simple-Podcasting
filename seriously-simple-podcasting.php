37a36a672f26798d8ade62b594edff7593df72e4
Sergiy Zakharchenko <zaharchenko.dev@gmail.com>
2024-01-15 14:45:02 +0000

    Version 3.0.0-beta.6

--- seriously-simple-podcasting.php
+++ seriously-simple-podcasting.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: Seriously Simple Podcasting
- * Version: 3.0.0-beta.5
+ * Version: 3.0.0-beta.6
  * Plugin URI: https://castos.com/seriously-simple-podcasting/?utm_medium=sspodcasting&utm_source=wordpress&utm_campaign=wpplugin_08_2019
  * Description: Podcasting the way it's meant to be. No mess, no fuss - just you and your content taking over the world.
  * Author: Castos
@@ -15,7 +15,6 @@
  * @package Seriously Simple Podcasting
  *
  * GitHub Plugin URI: https://github.com/CastosHQ/Seriously-Simple-Podcasting
- *
  */
 
 // Exit if accessed directly.
@@ -23,7 +22,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'SSP_VERSION', '3.0.0-beta.5' );
+define( 'SSP_VERSION', '3.0.0-beta.6' );
 define( 'SSP_PLUGIN_FILE', __FILE__ );
 define( 'SSP_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SSP_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );

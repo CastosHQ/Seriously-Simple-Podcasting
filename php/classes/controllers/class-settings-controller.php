4c40912569404f72a0dd49cdf3593c846f2340b5
Sergiy Zakharchenko <zaharchenko.dev@gmail.com>
2024-05-02 20:22:39 +0100

    PHP warning fixes

--- php/classes/controllers/class-settings-controller.php
+++ php/classes/controllers/class-settings-controller.php
@@ -156,7 +156,7 @@ class Settings_Controller {
 		if ( isset( $args['field']['id'] ) && 'podcasts_sync' === $args['field']['id'] ) {
 			$data = (array) $data;
 			$res = $this->castos_handler->get_podcasts();
-			if ( empty( $res['status'] ) || 'success' !== $res['status'] || ! isset( $res['data']['podcast_list'] ) ) {
+			if ( ! is_array( $res ) || empty( $res['status'] ) || 'success' !== $res['status'] || ! isset( $res['data']['podcast_list'] ) ) {
 				$data['statuses'] = null;
 
 				return $data;

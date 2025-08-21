a37d503fc43454d4d3f8713afdb91fcb19f28116
Serhiy Zakharchenko <zaharchenko.dev@gmail.com>
2025-08-21 12:25:15 +0100

    Tests

--- tests/wpunit/SSP_Functions_Test.php
+++ tests/wpunit/SSP_Functions_Test.php
@@ -223,15 +223,13 @@ class SSP_Functions_Test extends WPTestCase {
 		$this->assertCount( $series_number, $terms );
 
 		foreach ( $terms as $term ) {
-            $this->factory()->post->create(
+            $post_id = $this->factory()->post->create(
                 array(
                     'post_type'   => SSP_CPT_PODCAST,
                     'post_status' => 'publish',
-                    'tax_input'   => array( 
-						'series' => array( $term->term_id ) 
-					),
                 )
             );
+			wp_set_post_terms( $post_id, $term->term_id, 'series' );
 		}
 
 		$series = ss_get_podcast( $args );
